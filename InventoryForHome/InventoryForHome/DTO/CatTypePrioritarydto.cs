deca38c3debc0b3094ace0e1c13fca6a621a2f6e
Yazbeth <yazryrg@outlook.com>
2024-03-30 22:38:15 -0600

    +Actualización de la rama

---
+++ InventoryForHome/InventoryForHome/DTO/CatTypePrioritarydto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryForHome.DTO
+{
+    public class CatTypePrioritarydto
+    {
+        public string TypePrioritaryName { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ InventoryForHome/InventoryForHome/Models/Querys.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryForHome.Models
+{
+    public class Querys
+    {
+        public static async Task<List<Item>> ObtenerTablaItemAsync()
+        {
+            using (var db = new InventoryForHomeContext())
+            {
+                var Item = await db.Items.FromSql($"EXEC OptionMenu 2").ToListAsync();
+                return Item;
+            }
+        }
+    }
+}
